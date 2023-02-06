7c29b9abfa2abf5bd60417a241c38ecf19153e11
valentina vittoria <valeviit@icloud.com>
2023-02-06 13:03:54 +0100

    Joi Validation

--- src/routes/Author.ts
+++ src/routes/Author.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import { Schema } from "mongoose";
 import controllers from '../controllers/Author';
+import { Schemas, ValidateSchema } from "../middleware/ValidateSchema";
 
 const router = express.Router();
 
-router.post('/create', controllers.createAuthor);
+router.post('/create', ValidateSchema(Schemas.author.create), controllers.createAuthor);
 router.get('/get/:authorId', controllers.readAuthor);
 router.get('/get/', controllers.readAll);
-router.patch('/update/:authorId', controllers.updateAuthor);
+router.patch('/update/:authorId', ValidateSchema(Schemas.author.update), controllers.updateAuthor);
 router.delete('/delete/:authorId', controllers.deleteAuthor);
 
 
